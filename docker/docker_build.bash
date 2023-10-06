b24f64911a96b7c47061bbac597a72fa38466fe9
Fabio Ferrarini <ferrarini09@gmail.com>
2023-10-06 21:23:02 +0200

    Noetic Version

--- docker/docker_build.bash
+++ docker/docker_build.bash
@@ -4,18 +4,9 @@ MACHTYPE=`uname -m`
 
 UPAR="--build-arg UID=`id -u` --build-arg GID=`id -g`"
 
-if [ "$MACHTYPE" = "x86_64" ]; then
-  docker pull iocchi/orazio
-  #docker build -t orazio -f Dockerfile.orazio . && \
-  #docker tag orazio iocchi/orazio && \
-  #docker build -t iocchi/orazio:2018 -f Dockerfile.orazio2018 .
-else
-  docker pull iocchi/orazio:arm64
-  #docker build -t orazio -f Dockerfile.orazio . && \
-  #docker tag orazio iocchi/orazio:arm64 && \
-  #docker build -t iocchi/orazio:2018-arm64 -f Dockerfile.orazio2018 .
-fi && \
 docker build $UPAR -t marrtino:system -f Dockerfile.system . && \
+docker build $UPAR -t marrtino:noetic_system -f Dockerfile.noetic_system . && \
+docker build -t marrtino:orazio -f Dockerfile.orazio . && \
 docker build -t marrtino:base -f Dockerfile.base . && \
 docker build -t marrtino:teleop -f Dockerfile.teleop . && \
 docker build -t marrtino:navigation -f Dockerfile.navigation . && \
