dc3548c51a54aab2b1823209ecb0c1ba398d21f7
Fabio Ferrarini <ferrarini09@gmail.com>
2024-01-07 15:15:09 +0100

    Fix Gallery

--- gallery/static/script.js
+++ gallery/static/script.js
@@ -28,3 +28,41 @@ function do_close(){
   document.getElementById('top').hidden=true;
   document.getElementById('close').hidden=true;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const deleteButtons = document.querySelectorAll(".delete-btn");
+
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const path = button.getAttribute("data-path");
+
+      // Send a request to the server to delete the photo
+      fetch(`/delete-photo?path=${path}`, {
+        method: "DELETE",
+      })
+        .then(response => {
+          if (response.ok) {
+            // If the response is successful, remove the photo container from the DOM
+            const photoContainer = button.closest(".photo-container");
+            photoContainer.remove();
+          } else {
+            console.error("Failed to delete photo");
+          }
+        })
+        .catch(error => console.error("Error:", error));
+    });
+  });
+});
+
+function clicked_img(img) {
+  document.getElementById("top").src = img.src;
+  document.getElementById("top").hidden = false;
+  document.getElementById("close").hidden = false;
+  document.querySelector(".overlay").style.display = "flex";
+}
+
+function do_close() {
+  document.getElementById("top").hidden = true;
+  document.getElementById("close").hidden = true;
+  document.querySelector(".overlay").style.display = "none";
+}
