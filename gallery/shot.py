b43d61983292643fd097a20c6734602f51dbabd7
Fabio Ferrarini <ferrarini09@gmail.com>
2024-01-07 17:12:19 +0100

    Gallery

--- gallery/shot.py
+++ gallery/shot.py
@@ -1,9 +1,9 @@
 import sys
 import cv2
 
-def capture_photo(device_index=2, photo_filename='captured_photo.jpg'):
+def capture_photo(device_name, photo_filename='captured_photo.jpg'):
     # Open the video capture device
-    cap = cv2.VideoCapture(device_index)
+    cap = cv2.VideoCapture(device_name)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 4600)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 2592)
 
@@ -40,9 +40,9 @@ def main():
     # Your program logic here
     #print(f"The parameter passed is: {parameter}")
     # Specify the device index and photo filename (modify as needed)
-    device_index = 2  # Check which device index your camera is using (e.g., /dev/video2)
+    device_name = "video0"  # Check which device index your camera is using (e.g., /dev/video2)
     # Capture the photo
-    capture_photo(device_index, photo_filename)
+    capture_photo(device_name, photo_filename)
 
     
 if __name__ == "__main__":
