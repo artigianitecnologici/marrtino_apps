0493603d626dfbeec8f787e6c86b64cfe9a85315
Fabio Ferrarini <ferrarini09@gmail.com>
2024-09-23 19:05:45 +0200

    Update TTS

--- bringup/speech_bringup.py
+++ bringup/speech_bringup.py
@@ -76,6 +76,9 @@ def run_server(port):
                 if data=='@audio':
                     tmux.cmd(0,'cd %s' %folder)
                     tmux.cmd(0,'python audio_server.py')
+                    tmux.cmd(2,'cd %s' %folder)
+                    tmux.cmd(2,'python node_tts.py')
+                    
                 elif data=='@audiokill':
                     tmux.Cc(0)
                 else:
