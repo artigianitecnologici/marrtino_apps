e3aa2d3e9be5eeeb3b44adf4c0fba879ae204367
Fabio Ferrarini <ferrarini09@gmail.com>
2024-10-19 20:52:07 +0200

    Update autostart

--- start/autostart.py
+++ start/autostart.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os, sys, time
+import logging
 try:
     import yaml
 except Exception as e:
@@ -12,6 +13,9 @@ except Exception as e:
 import subprocess
 import re
 
+# Configura il logging
+log_filename = "autostart_log_{}.log".format(time.strftime("%Y%m%d-%H%M%S"))
+logging.basicConfig(filename=log_filename, level=logging.INFO, format='%(asctime)s - %(message)s')
 
 def find_webcam_by_name(device_name):
     # Run v4l2-ctl to list video devices
@@ -43,152 +47,143 @@ def readconfig(yamlfile):
 
 
 def systemcmd(cmdkey, port):
-    cmd = "echo '%s' | netcat -w 1 localhost %d" %(cmdkey,port)
-    print("  "+cmd)
+    cmd = "echo '%s' | netcat -w 1 localhost %d" % (cmdkey, port)
+    print("  " + cmd)
+    logging.info("Running systemcmd: %s", cmd)  # Log ogni chiamata di sistema
     os.system(cmd)
     time.sleep(3)
-    
 
-def getconfig(section,key):
+def getconfig(section, key):
     try:
         r = config[section][key]
     except:
-        #print("No value for %s:%s" %(section,key))
+        # print("No value for %s:%s" %(section,key))
         r = None
     return r
 
 def autostart(config, dostart):
+    logging.info("Autostart initiated. Start mode: %s", "Start" if dostart else "Kill")
     print("auto start:")
 
-    #  simulator
-    if getconfig('simulator','stage'):
-        mapname = getconfig('simulator','mapname')
-        mapdir = getconfig('simulator','mapdir')
-        robottype = getconfig('simulator','robottype')
-        nrobots = getconfig('simulator','nrobots')
-        if mapdir!='default':
-            print("TODO: set mapdir to %s !!!" %mapdir)
+    # simulator
+    if getconfig('simulator', 'stage'):
+        mapname = getconfig('simulator', 'mapname')
+        mapdir = getconfig('simulator', 'mapdir')
+        robottype = getconfig('simulator', 'robottype')
+        nrobots = getconfig('simulator', 'nrobots')
+        if mapdir != 'default':
+            print("TODO: set mapdir to %s !!!" % mapdir)
         simtime = 'rosparam set /use_sim_time '
         simtimeval = 'true' if dostart else 'false'
         simtime = simtime + simtimeval
-        cmd = 'docker exec -it base bash -ci "%s"' %simtime
+        cmd = 'docker exec -it base bash -ci "%s"' % simtime
         os.system(cmd)
-        cmd = "%s;%s;%d" %(mapname,robottype,nrobots) if dostart else "@stagekill"
-        systemcmd(cmd,9235)
+        cmd = "%s;%s;%d" % (mapname, robottype, nrobots) if dostart else "@stagekill"
+        systemcmd(cmd, 9235)
 
     # devices
-    if getconfig('devices','robot'):
+    if getconfig('devices', 'robot'):
         cmd = '@robot' if dostart else '@robotkill'
-        systemcmd(cmd,9236)
-    if getconfig('devices','joystick') == '2wd':
+        systemcmd(cmd, 9236)
+    if getconfig('devices', 'joystick') == '2wd':
         cmd = '@joystick' if dostart else '@joystickkill'
-        systemcmd(cmd,9240)
-    if getconfig('devices','joystick') == 'keyboard':
+        systemcmd(cmd, 9240)
+    if getconfig('devices', 'joystick') == 'keyboard':
         cmd = '@keyboard' if dostart else '@joystickkill'
-        systemcmd(cmd,9240)
-    elif getconfig('devices','joystick') == '4wd':
+        systemcmd(cmd, 9240)
+    elif getconfig('devices', 'joystick') == '4wd':
         cmd = '@joystick4wd' if dostart else '@joystickkill'
-        systemcmd(cmd,9240)
-    cam = getconfig('devices','camera') 
-    if cam=='usbcam' or cam=='astra' or cam=='xtion':
-        cmd = '@%s' %cam if dostart else '@camerakill'
-        systemcmd(cmd,9237)
-    else :
+        systemcmd(cmd, 9240)
+    cam = getconfig('devices', 'camera')
+    if cam == 'usbcam' or cam == 'astra' or cam == 'xtion':
+        cmd = '@%s' % cam if dostart else '@camerakill'
+        systemcmd(cmd, 9237)
+    else:
         device_name_to_find = "USB 2.0 Camera"
         # Find and print information about the webcam
         device_name = find_webcam_by_name(device_name_to_find)
         cmd = '@camera_' + device_name
         print(cmd)
-        systemcmd(cmd,9237)
+        systemcmd(cmd, 9237)
 
-    #if cam=='shot':
-    #    cmd = '@%s' %cam if dostart else '@camerakill'
-    #    systemcmd(cmd,9253)
-    #if cam=='d345' :
-    #    cmd = '@%s' %cam if dostart else '@camerakill'
-    #    systemcmd(cmd,9237)  
-    las = getconfig('devices','laser')
-    if  las=='hokuyo' or las=='rplidar' or las=='ld06':
-        cmd = '@%s' %las if dostart else '@laserkill'
-        systemcmd(cmd,9238)
-
+    las = getconfig('devices', 'laser')
+    if las == 'hokuyo' or las == 'rplidar' or las == 'ld06':
+        cmd = '@%s' % las if dostart else '@laserkill'
+        systemcmd(cmd, 9238)
 
     # functions
-    if getconfig('functions','localization'):
+    if getconfig('functions', 'localization'):
         cmd = '@loc' if dostart else '@lockill'
-        systemcmd(cmd,9238)
-    if getconfig('functions','navigation') == 'gbn':
+        systemcmd(cmd, 9238)
+    if getconfig('functions', 'navigation') == 'gbn':
         cmd = '@gbn' if dostart else '@gbnkill'
-        systemcmd(cmd,9238)
-    elif getconfig('functions','navigation') == 'move_base':
+        systemcmd(cmd, 9238)
+    elif getconfig('functions', 'navigation') == 'move_base':
         cmd = '@movebase' if dostart else '@movebasekill'
-        systemcmd(cmd,9238)
-    elif getconfig('functions','navigation') == 'move_base_gbn':
+        systemcmd(cmd, 9238)
+    elif getconfig('functions', 'navigation') == 'move_base_gbn':
         cmd = '@movebasegbn' if dostart else '@movebasekill'
-        systemcmd(cmd,9238)
-    if getconfig('functions','navigation_rviz'):
+        systemcmd(cmd, 9238)
+    if getconfig('functions', 'navigation_rviz'):
         cmd = '@rviz' if dostart else '@rvizkill'
-        systemcmd(cmd,9238)
-    if getconfig('functions','mapping') == 'gmapping':
+        systemcmd(cmd, 9238)
+    if getconfig('functions', 'mapping') == 'gmapping':
         cmd = '@gmapping' if dostart else '@gmappingkill'
-        systemcmd(cmd,9241)
-    elif getconfig('functions','mapping') == 'srrg_mapper':
+        systemcmd(cmd, 9241)
+    elif getconfig('functions', 'mapping') == 'srrg_mapper':
         cmd = '@srrgmapper' if dostart else '@srrgmapperkill'
-        systemcmd(cmd,9241)
-    if getconfig('functions','mapping_rviz'):
+        systemcmd(cmd, 9241)
+    if getconfig('functions', 'mapping_rviz'):
         cmd = '@rviz' if dostart else '@rvizkill'
-        systemcmd(cmd,9241)
+        systemcmd(cmd, 9241)
 
-    if getconfig('functions','videoserver'):
+    if getconfig('functions', 'videoserver'):
         cmd = '@videoserver' if dostart else '@videoserverkill'
-        systemcmd(cmd,9237)
-    if getconfig('functions','rosbridge'):
+        systemcmd(cmd, 9237)
+    if getconfig('functions', 'rosbridge'):
         cmd = '@rosbridge' if dostart else '@rosbridgekill'
-        systemcmd(cmd,9237)
-    if getconfig('functions','gallery'):
+        systemcmd(cmd, 9237)
+    if getconfig('functions', 'gallery'):
         cmd = '@gallery' if dostart else '@gallerykill'
-        systemcmd(cmd,9253)
+        systemcmd(cmd, 9253)
         cmd = '@shotnode' if dostart else '@shotnodekill'
-        systemcmd(cmd,9253)
-    if getconfig('functions','apriltags'):
+        systemcmd(cmd, 9253)
+    if getconfig('functions', 'apriltags'):
         cmd = '@apriltags' if dostart else '@apriltagskill'
-        systemcmd(cmd,9237)
-    if getconfig('functions','objrec'):
+        systemcmd(cmd, 9237)
+    if getconfig('functions', 'objrec'):
         cmd = '@objrec' if dostart else '@objreckill'
-        systemcmd(cmd,9242)
-    if getconfig('functions','audioserver'):
+        systemcmd(cmd, 9242)
+    if getconfig('functions', 'audioserver'):
         cmd = '@audio' if dostart else '@audiokill'
-        systemcmd(cmd,9239)
-    soc = getconfig('functions','social')
-    if soc=='on':
-        soc='social'
-    if soc=='social':
+        systemcmd(cmd, 9239)
+    soc = getconfig('functions', 'social')
+    if soc == 'on':
+        soc = 'social'
+    if soc == 'social':
         cmd = '@robotsocial' if dostart else '@socialkill'
-        systemcmd(cmd,9250)
-    elif soc=='notracker':
+        systemcmd(cmd, 9250)
+    elif soc == 'notracker':
         cmd = '@socialnotracker' if dostart else '@socialkill'
-        systemcmd(cmd,9250)
-    elif soc=='noservo':
-        cmd = '@socialnoservo' if dostart else '@socialkill'  
-        systemcmd(cmd,9250)
+        systemcmd(cmd, 9250)
+    elif soc == 'noservo':
+        cmd = '@socialnoservo' if dostart else '@socialkill'
+        systemcmd(cmd, 9250)
 
-    if getconfig('functions','pantilt'):
+    if getconfig('functions', 'pantilt'):
         cmd = '@pantilt_start' if dostart else '@pantilt_kill'
-        systemcmd(cmd,9249)
+        systemcmd(cmd, 9249)
 
-#
-
-# Use python autostart.py [start.yaml file] [--kill]
-
-if __name__=='__main__':
+# Main program
+if __name__ == '__main__':
 
     autostartfile = "autostart.yaml"
-    yamlfile = os.getenv('MARRTINO_APPS_HOME')+"/"+autostartfile
+    yamlfile = os.getenv('MARRTINO_APPS_HOME') + "/" + autostartfile
     dostart = True
 
-    if len(sys.argv)>1 and sys.argv[1]=='--kill':
+    if len(sys.argv) > 1 and sys.argv[1] == '--kill':
         dostart = False
-    if len(sys.argv)>2 and sys.argv[2]=='--kill':
+    if len(sys.argv) > 2 and sys.argv[2] == '--kill':
         dostart = False
     if len(sys.argv)>1 and sys.argv[1][0]!='-':
         yamlfile = sys.argv[1]
@@ -203,5 +198,4 @@ if __name__=='__main__':
 
     config = readconfig(yamlfile)
     autostart(config, dostart)
-
 
